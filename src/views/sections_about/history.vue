601d5ccb13a2ba3a72d62dd40cc7bc877cdfad32
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-01-13 21:00:29 -0500

    Fix #4

--- src/views/sections_about/history.vue
+++ src/views/sections_about/history.vue
@@ -1,86 +1,77 @@
 <script>
-import firebase from 'firebase';
+import firebase from "firebase";
 
 export default {
+  created() {
+    const firestore = firebase.firestore();
 
-  created(){
-
-   const firestore = firebase.firestore();
-
-    var timeline_db = firestore.collection('Timeline');
-    timeline_db.get().then(snapshot => {
-
-      
-      //Loop over timeline-dates
-      snapshot.forEach(doc => {
-        const rawDateFromTimeline =  doc.data();
-        this.addDateToTimeline(rawDateFromTimeline);
-      });
-
-    }).catch( error => {
+    var timeline_db = firestore.collection("Timeline");
+    timeline_db
+      .get()
+      .then(snapshot => {
+        //Loop over timeline-dates
+        snapshot.forEach(doc => {
+          const rawDateFromTimeline = doc.data();
+          this.addDateToTimeline(rawDateFromTimeline);
+        });
+        this.TIMELINES.map(timeline => {
+          timeline.dates.reverse();
+        });
+      })
+      .catch(error => {
         console.error(error);
-    });
-
+      });
   },
 
-  methods:{
-    
+  methods: {
     addDateToTimeline(rawData) {
-      
       const rawTimeline = {
-        id: ( new Date() ).getTime(),
-        date : rawData.date.toDate(),
+        id: 100000000000 - new Date(rawData.date.toDate()).getTime(),
+        date: rawData.date.toDate(),
         src: rawData.image,
-        text : rawData.text
+        text: rawData.text
       };
 
       const rawYear = rawTimeline.date.getFullYear();
 
-      this.TIMELINES.map( timeline => {
-        if(timeline.year == rawYear){
+      this.TIMELINES.map(timeline => {
+        if (timeline.year == rawYear) {
           timeline.dates.push(rawTimeline);
         }
       });
-
-
-
     }
-
   },
 
-  data(){
-    return{
-        TIMELINES : [
+  data() {
+    return {
+      TIMELINES: [
+        {
+          year: 2015,
+          color: "green",
+          dates: []
+        },
 
-    
-          {
-            year: 2015,
-            color : 'green',
-            dates: []
-          },
+        {
+          year: 2016,
+          color: "orange",
+          dates: []
+        },
 
-          {
-            year: 2016,
-            color : 'orange',
-            dates: []
-          },
+        {
+          year: 2017,
+          color: "red",
+          dates: []
+        },
 
-          {
-            year: 2017,
-            color : 'red',
-            dates: []
-          },
-
-          {
-            year: 2018,
-            color : 'yellow',
-            dates: []
-          }
-          
-        ]
-    }
+        {
+          year: 2018,
+          color: "yellow",
+          dates: []
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 
@@ -88,153 +79,158 @@ export default {
 <template>
   <div class="timeline" id="about-history">
     <div class="timeline-wrapper">
-
-      
-
       <div class="timelime-item" v-for="kTimeline in TIMELINES" v-bind:key="kTimeline.year">
-       
+        <div
+          class="timeline-item-type-content"
+          v-for="kDates in kTimeline.dates "
+          v-bind:key="kDates.id"
+        >
+          <div class="col dates" :class="['gi-'+kTimeline.color]">
+            <h4 v-if="kDates.id==0">{{ kDates.date.getFullYear() }}</h4>
+          </div>
 
-        <div class="timeline-item-type-content" v-for="kDates in kTimeline.dates" v-bind:key="kDates.id">
-              
-              <div class="col dates" :class="['gi-'+kTimeline.color]"> 
-                <h4>{{ kDates.date.getFullYear() }}</h4>
-              </div>
-              
-              <div class="col thumbs"> 
-                <img :src="kDates.src" width="100%">
-              </div>
-              
-              <div class="col text">
-                <p>
-                  {{ kDates.text }}
-                </p>
-              </div>
+          <div class="col thumbs">
+            <img :src="kDates.src" width="100%">
+          </div>
 
+          <div class="col text">
+            <p>{{ kDates.text }}</p>
+          </div>
         </div>
 
         <div class="timeline-item-type-division">
-          
-            <img class="div-icon div-icon-green" src="../../assets/timeline/DIV_0.png" alt="" v-if="kTimeline.color=='green'" width="100%">
-            <img class="div-icon div-icon-orange" src="../../assets/timeline/DIV_1.png" alt="" v-if="kTimeline.color=='orange'" width="100%">
-            <img class="div-icon div-icon-red" src="../../assets/timeline/DIV_2.png" alt="" v-if="kTimeline.color=='red'" width="100%">
-            <img class="div-icon div-icon-yellow" src="../../assets/timeline/DIV_3.png" alt="" v-if="kTimeline.color=='yellow'" width="100%">
-
+          <img
+            class="div-icon div-icon-green"
+            src="../../assets/timeline/DIV_0.png"
+            alt
+            v-if="kTimeline.color=='green'"
+            width="100%"
+          >
+          <img
+            class="div-icon div-icon-orange"
+            src="../../assets/timeline/DIV_1.png"
+            alt
+            v-if="kTimeline.color=='orange'"
+            width="100%"
+          >
+          <img
+            class="div-icon div-icon-red"
+            src="../../assets/timeline/DIV_2.png"
+            alt
+            v-if="kTimeline.color=='red'"
+            width="100%"
+          >
+          <img
+            class="div-icon div-icon-yellow"
+            src="../../assets/timeline/DIV_3.png"
+            alt
+            v-if="kTimeline.color=='yellow'"
+            width="100%"
+          >
         </div>
-
-
       </div>
-      
-    </div>    
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-h2{
-  color:black !important;
+h2 {
+  color: black !important;
 }
 
-.timeline-item-type-content{
+.timeline-item-type-content {
   display: block;
-  .thumbs{
+  .thumbs {
     width: 12em;
-    height: 8em;;
+    height: 8em;
     display: inline-block !important;
     vertical-align: top;
     float: left;
-    img{
-          margin-top: 1em;
+    img {
+      margin-top: 1em;
     }
   }
-  .dates{
+  .dates {
     width: 12em;
     height: 8em;
     float: left;
     display: inline-block !important;
     vertical-align: top;
   }
-  .text{
+  .text {
     width: calc(100% - 25em);
-    height: 8em;;
+    height: 8em;
     display: inline-block !important;
     vertical-align: top;
   }
 }
 
-
-
-
-.thumbs,.dates,.text{
+.thumbs,
+.dates,
+.text {
   display: inline-block;
 }
 
-
-
-
-
-
-
-.thumbs,.div-icon,.dates{
+.thumbs,
+.div-icon,
+.dates {
   display: block;
   width: 12em;
   text-align: center;
 }
 
+.timeline-item-type-division {
+  width: calc(8em +0px);
+  padding-left: 0px;
 
-.timeline-item-type-division{
-          width: calc(8em +0px);
-          padding-left: 0px;
-          
-      img{
-        width: calc(8em + 0px);
-        padding-left: 0px;
-       
-      }
+  img {
+    width: calc(8em + 0px);
+    padding-left: 0px;
+  }
 }
-.dates{
-
+.dates {
   width: 8em !important;
 
   padding-top: 1em;
   padding-bottom: 1em;
 
-      display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-flex-direction: column;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    -webkit-flex-wrap: nowrap;
-    -ms-flex-wrap: nowrap;
-    flex-wrap: nowrap;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    -webkit-align-content: center;
-    -ms-flex-line-pack: center;
-    align-content: center;
-    -webkit-align-items: center;
-    -ms-flex-align: center;
-    align-items: center;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-flex-wrap: nowrap;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-align-content: center;
+  -ms-flex-line-pack: center;
+  align-content: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
 
-  h4{
+  h4 {
     font-weight: 200;
     text-align: center;
     margin: 0 auto;
   }
-  &.gi-green{
-        background-color: #8bc53f !important;
+  &.gi-green {
+    background-color: #8bc53f !important;
   }
-  &.gi-orange{
-        background-color: #e5520f !important;
+  &.gi-orange {
+    background-color: #e5520f !important;
   }
-  &.gi-red{
-        background-color: #961934 !important;
+  &.gi-red {
+    background-color: #961934 !important;
   }
-  &.gi-yellow{
-        background-color: #ffb90f !important;
+  &.gi-yellow {
+    background-color: #ffb90f !important;
   }
 }
-.text{
-
+.text {
 }
 </style>
